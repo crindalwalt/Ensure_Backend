5c6196bf7404df9aa9f7a07ca8c3bd5f16293fa9
Shahzad Farooq <crindalwalt@protonmail.com>
2023-03-22 19:56:56 +0500

    operators done

---
+++ 1-variable.php
@@ -0,0 +1,23 @@
+<?php
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++
+//      Variable    
+//++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+
+// let vari = 3847; // IN JS
+$name = "Shahzad Farooq";
+$name = 67;
+$name = array("Shahzad","Ahmed","Husain","Ali");
+
+/**
+ * DataTypes
+ *      String
+ *      Number
+ *      Array
+ * 
+ */
+
+// echo var_dump( $name); // see inspection
+?>
---
+++ 2-operators.php
@@ -0,0 +1,39 @@
+<?php
+//+++++++++++++++++++++++++++++++++++++++++++++++++
+//      Operators    
+//++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+
+/**? Type of Operators
+ * 
+ * 
+ *  Arithmatic operator (+,-,*,/,%,**)
+ *  Comparison operator (<,>,<=,>=,==,===,!=)
+ *  Assignment operator (=,+=,.=,-=,*=,/=)
+ *  Logical operator (or,and,not)
+ * 
+ * 
+ * 
+ */
+
+
+
+$num1 = 23;
+$num2 = 44;
+
+
+// $num1 += $num2;
+$expression = $num1 > $num2 or $num1 < 20;
+echo var_dump(!$expression);
+
+// $result =  $num1 != $num2;
+// echo var_dump($result);
+
+
+// $greeting = "Hello ";
+// $name = "Danial";
+// echo $greeting.$name;
+// echo 25**2;
+
+
